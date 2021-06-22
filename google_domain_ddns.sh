d3d4749a0c7d6fb36c7bc20fe3c22b2b9fb34576
Lucas Alcântara <lucasapd@gmail.com>
2021-06-21 22:13:37 -0300

    Update google_domain_ddns.sh

--- google_domain_ddns.sh
+++ google_domain_ddns.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 # Google Domains provides an API to update a DNS:
-# https://support.google.com/domains/answer/6147083
+# First, create a entry and user/password:
+  #  ttps://support.google.com/domains/answer/6147083
 
 # How to:
 # 1 - Save file:
-#   wget -c /home/$USER/ https://raw.githubusercontent.com/lucas-apd/scripts/main/google_domain_ddns.sh 
+  #   wget -c /home/$USER/ https://raw.githubusercontent.com/lucas-apd/scripts/main/google_domain_ddns.sh 
 # 2 - Permission set: 
-#   chmod +x /home/$USER/google_domain_ddns.sh
+  #   chmod +x /home/$USER/google_domain_ddns.sh
 # 3 - Create a cronjob for auto update (every 30 minutes):
-#   { crontab -l; echo "*/30 * * * * /home/$USER/google_domain_ddns.sh"; } | crontab -
+  #   { crontab -l; echo "*/30 * * * * /home/$USER/google_domain_ddns.sh"; } | crontab -
 # 4 - Edit personal information:
-#   cd /home/$USER/ && vi google_domain_ddns.sh
+  #   cd /home/$USER/ && vi google_domain_ddns.sh
+
+# Start changing... 
 
 USERNAME=""
 PASSWORD=""
 DNS_ENDPOINT=""
+
+# STOP !!!
+
 PUBLIC_IP=$(curl -s https://checkip.amazonaws.com)
 URL="https://${USERNAME}:${PASSWORD}@domains.google.com/nic/update?hostname=${DNS_ENDPOINT}&myip=${PUBLIC_IP}"
 
