1a3b3f5e16c28d2a6f68c6f0acad57f8d55180a9
Lucas Alcântara <lucasapd@gmail.com>
2021-11-08 17:09:49 -0300

    Update filter_dependency_check_report.sh

--- filter_dependency_check_report.sh
+++ filter_dependency_check_report.sh
@@ -1,1 +1,4 @@
-q -r '[.dependencies[] | select(.evidenceCollected[] != []) | select( .vulnerabilities[0].severity == "HIGH" or .vulnerabilities[0].severity == "CRITICAL" ) | select(.fileName | contains("shaded") | not) | {Dependency: .fileName, Path: .filePath, Version: .evidenceCollected.versionEvidence[0].value, CVE: [.vulnerabilities[].name] , CWE: [.vulnerabilities[].cwes[]], Severity: .vulnerabilities[0].severity, Description: [.vulnerabilities[].description], Reference: .vulnerabilityIds[0].url }] | unique_by(.Dependency)' dependency_check.json
+q -r '[.dependencies[] | select(.evidenceCollected[] != []) | select( .vulnerabilities[0].severity == "HIGH" or .vulnerabilities[0].severity == "CRITICAL" ) \
+| select(.fileName | contains("shaded") | not) | {Dependency: .fileName, Path: .filePath, Version: .evidenceCollected.versionEvidence[0].value, \
+CVE: [.vulnerabilities[].name] , CWE: [.vulnerabilities[].cwes[]], Severity: .vulnerabilities[0].severity, Description: [.vulnerabilities[].description], \ 
+Reference: .vulnerabilityIds[0].url }] | unique_by(.Dependency)' dependency_check.json
